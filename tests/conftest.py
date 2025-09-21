798bdf1eb52aa916a932675143ee89c09e1dde3b
Aleksey Pavlovskii <agp251093@gmail.com>
2025-09-21 18:42:03 +0300

    Сделаны тесты и покрытие

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ def first_category() -> Category:
     return Category(
         name="Смартфоны",
         description="Смартфоны, как средство не только коммуникации, "
-        "но и получение дополнительных функций для удобства жизни",
+                    "но и получение дополнительных функций для удобства жизни",
         products=[
             Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет 200MP камера", 180000.0, 5),
             Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
@@ -22,7 +22,7 @@ def second_category() -> Category:
     return Category(
         name="Телевизоры",
         description="Современный телевизор, который позволяет наслаждаться просмотром, "
-        "станет вашим другом и помощником",
+                    "станет вашим другом и помощником",
         products=[Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)],
     )
 
@@ -38,9 +38,22 @@ def dict_category() -> list[dict]:
         {
             "name": "Телевизоры",
             "description": "Современный телевизор, который позволяет наслаждаться просмотром, "
-            "станет вашим другом и помощником",
+                           "станет вашим другом и помощником",
             "products": [
                 {"name": '55" QLED 4K', "description": "Фоновая подсветка", "price": 123000.0, "quantity": 7}
             ],
         }
+    ]
+
+
+@pytest.fixture
+def new_product() -> dict:
+    return {"name": "Xiaomi Redmi Note 11", "description": "1024GB, Синий", "price": 31000.0, "quantity": 14}
+
+
+@pytest.fixture
+def list_products() -> list:
+    return [
+        Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет 200MP камера", 180000.0, 5),
+        Product("Xiaomi Redmi Note 11", "1024GB, Синий", 21000.0, 7),
     ]
