a9551cf6dcbdd3ed7a9684fb003b3f1c25fb8b91
Aleksey Pavlovskii <agp251093@gmail.com>
2025-09-21 18:41:38 +0300

    Добавлен новый функционал

--- src/category_products.py
+++ src/category_products.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class Category:
     """Класс для представления категорий товаров."""
 
@@ -11,6 +14,22 @@ class Category:
         """Метод, который инициализирует экземпляры класса."""
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    @property
+    def products(self) -> str:
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_str
+
+    def add_product(self, product: Any) -> None:
+        """Метод, который добавляет новый товар в список товаров"""
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products_in_list(self) -> list:
+        return self.__products
